996b00942ddb0f9b9d0aa4322da67ed96f5c768e
thinkty <the.thinkty@gmail.com>
2023-02-07 13:44:19 -0500

    Handling POST request from the webpage and printing the contents

--- main/app_http.h
+++ main/app_http.h
@@ -10,10 +10,17 @@
 #endif
 
 /**
- * URI handler for root (/).
+ * URI handler for root (/) GET request.
  * Respond with the webpage to upload an image.
  */
 esp_err_t get_handler(httpd_req_t * req);
+
+/**
+ * URI handler for root (/) POST request.
+ * Read the binary image from the request.
+ * Respond with an adequate page to show the result.
+ */
+esp_err_t post_handler(httpd_req_t * req);
 
 /**
  * Start the http daemon in background on the default port
